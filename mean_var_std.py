bb83ff0651ca1760d0da7f4d8c1aaa47cf945c4e
Eduardo Vilchis Lugo <121980422+LaloVilchis24@users.noreply.github.com>
2025-07-11 20:55:19 -0600

    Update mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,21 @@
 import numpy as np
 
 def calculate(list):
+    # Execption
+    if len(list) != 9:
+        raise ValueError("List must contain nine numbers.")
 
+    # Frist transform list to NumPy matrix 3x3
+    mtx = np.array(list).reshape(3,3)
 
+    # Create dicctionary using tolist (python list)
+    calculations = { 'mean' : [mtx.mean(axis = 0).tolist(), mtx.mean(axis = 1).tolist(), mtx.mean().tolist()],
+                    'variance': [mtx.var(axis = 0).tolist(), mtx.var(axis = 1).tolist(), mtx.var().tolist()],
+                    'standard deviation': [mtx.std(axis = 0).tolist(), mtx.std(axis = 1).tolist(), mtx.std().tolist()],
+                    'max': [mtx.max(axis = 0).tolist(), mtx.max(axis = 1).tolist(), mtx.max().tolist()],
+                    'min': [mtx.min(axis = 0).tolist(), mtx.min(axis = 1).tolist(), mtx.min().tolist()],
+                    'sum': [mtx.sum(axis = 0).tolist(), mtx.sum(axis = 1).tolist(), mtx.sum().tolist()]
+    }
 
 
     return calculations
